33da604fe51da7fd18474adab505e011e40d511c
xteriaclan <wakyyak@gmail.com>
2026-04-08 16:36:49 +0200

    Update content.js

--- js/content.js
+++ js/content.js
@@ -76,6 +76,7 @@ export async function fetchLeaderboard() {
 
         // Records
         level.records.forEach((record) => {
+            if (!record.user) return;
             const user = Object.keys(scoreMap).find(
                 (u) => u.toLowerCase() === record.user.toLowerCase(),
             ) || record.user;
